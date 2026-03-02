ec8bbf968aa8bea50436708640a6f8c06d36063c
Knight <liujunfeng242@gmail.com>
2026-03-02 14:06:20 +0800

    redis替代session代码开发

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.utils.MessageConstant;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
@@ -10,11 +14,17 @@ import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -29,6 +39,8 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     /**
      * 发送验证码
      *
@@ -42,8 +54,8 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         }
         //2.生成验证码
         String code = RandomUtil.randomNumbers(6);
-        //3.保存验证码到session
-        session.setAttribute(MessageConstant.CODE, code);
+        //3.保存验证码到redis
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
         //4.发送验证码
         log.debug("发送验证码成功，验证码：{}", code);
 
@@ -57,32 +69,39 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
      */
     public Result login(LoginFormDTO loginForm, HttpSession session) {
         //1.校验手机号
-        if (RegexUtils.isPhoneInvalid(loginForm.getPhone())) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
             return Result.fail("手机号格式错误");
         }
         //2.校验验证码
         String code = loginForm.getCode();
-        String cacheCode = session.getAttribute(MessageConstant.CODE).toString();
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
         if (cacheCode == null || !cacheCode.equals(code)) {
             return Result.fail("验证码错误");
         }
         //3.是否存在用户
-        String phone = loginForm.getPhone();
         User user = query().eq("phone", phone).one();
         //4.不存在，创建用户
         if (user == null) {
             user = createUserWithPhone(phone);
         }
-        //5.保存用户到session
-        session.setAttribute(MessageConstant.USER, user);
-        return Result.ok();
+        //5.保存用户到redis
+        String token = UUID.randomUUID().toString();
+        String Key = LOGIN_USER_KEY + token;
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        stringRedisTemplate.opsForHash().putAll(Key,BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create().setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName,fieldValue)
+                                -> fieldValue.toString())));
+        stringRedisTemplate.expire(Key,LOGIN_USER_TTL,TimeUnit.MINUTES);
+        return Result.ok(token);
     }
 
 
     private User createUserWithPhone(String phone) {
         User user = new User();
         user.setPhone(phone);
-        user.setNickName(USER_NICK_NAME_PREFIX + "大天才");
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
         save(user);
         return user;
     }
