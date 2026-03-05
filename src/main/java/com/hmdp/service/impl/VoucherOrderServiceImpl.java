57d0c04e3cab6dcd47a8564bf4c0c799e43ad6fd
Knight <liujunfeng242@gmail.com>
2026-03-05 10:45:04 +0800

    通过悲观锁来实现单服务器的一人一单

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -9,8 +9,10 @@ import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -45,17 +47,33 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (seckillVoucher.getStock() < 1) {
             return Result.fail("库存不足");
         }
-        //5.减去库存更新
+        //5.减去库存并且创建订单
+        Long id = UserHolder.getUser().getId();
+        synchronized (id.toString().intern()){
+            IVoucherOrderService proxy =  (IVoucherOrderService)AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        Long id = UserHolder.getUser().getId();
+        int count = query().eq("user_id", id).eq("voucher_id", voucherId).count();
+        if(count > 0){
+            return Result.fail("已下一单，不能重复下单");
+        }
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1").setSql("update_time = NOW()" )
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId).gt("stock", 0).update();
         if(!success){
             return Result.fail("库存不足");
         }
         //6.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
+
         voucherOrder.setId(redisIdWorker.nextId("order"))
-                .setUserId(UserHolder.getUser().getId())
+                .setUserId(id)
                 .setVoucherId(voucherId);
         save(voucherOrder);
         //7.返回订单Id
